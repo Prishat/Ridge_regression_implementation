043a8bdae11e7098b2b950594b0eb1dd7fc0a227
PRISHAT BACHHAR <37043631+Prishat@users.noreply.github.com>
2019-05-11 02:19:47 +0530

    Add files via upload

--- Ridge_implementation.py
+++ Ridge_implementation.py
@@ -17,6 +17,10 @@ with open('Train_f.csv') as fp:
 
 data.dropna(inplace=True)   
 x=np.array(data.drop(["Loan_ID","11"],axis=1))
+
+"""To account for the constant term"""
+x=np.hstack((x,np.zeros([x.shape[0],1])))
+
 y=np.array(data["11"].values)
 y=y.reshape(y.shape[0],1)
 
@@ -44,10 +48,12 @@ def _gradient_descent(x,y,w,alpha,beta):
     update=(alpha/m)*(np.dot(x.T,(x.dot(w)-y)) + beta*w)
     w-=update
     return w,C
-    
+
+"""Stochastic gradient descent"""    
 def _fit_gd(x,y,alpha,beta,epochs):
     x=_standardize(x)
     y=_standardize(y)
+    
     w=np.zeros([x.shape[1],1])
     loss=[]
     for i in range(epochs):
@@ -56,6 +62,36 @@ def _fit_gd(x,y,alpha,beta,epochs):
         w=loss[i][0]
         
     return w
+
+def _fit_sgd(x,y,alpha,beta,epochs):
+    x=_standardize(x)
+    y=_standardize(y)
+    w=np.zeros([x.shape[1],1])
+    for i in range(epochs):
+        for j in range(len(x)):
+            C=_cost(x,y,w,beta)
+            xi=x[j].reshape(1,x.shape[1])
+            update=(alpha)*(xi.T.dot(xi.dot(w)-y[j]) + beta*w)
+            w-=update
+        
+    return w
+"""mini-batch gradient descent"""
+def _fit_mb_gd(x,y,alpha,beta,epochs,batch_size):
+    x=_standardize(x)
+    y=_standardize(y)
+    w=np.zeros([x.shape[1],1])
+    loss=[]
+    j=0
+    for i in range(0,x.shape[0],batch_size):
+        xi=x[i:i+batch_size]
+        #xi=xi.reshape(batch_size,x.shape[1])
+        yi=y[i:i+batch_size]
+        loss.append(_gradient_descent(xi,yi,w,alpha,beta))
+        w=loss[j][0]
+        j+=1
+        
+    return w
+    
 
 def _predict(x,w):
     y_hat=x.dot(w)
@@ -79,5 +115,14 @@ def _accuracy(y,y_hat):
 w_1=_fit_gd(x_train,y_train,0.1,1,1000)
 y_1=_predict(x_val,w_1)
 acc_1=_accuracy(y_val,y_1)
-print(acc_1,"%") 
-   
+print("GD: ",acc_1,"%") 
+
+w_2=_fit_sgd(x_train,y_train,0.3,0.2,100)
+y_2=_predict(x_val,w_2)
+acc_2=_accuracy(y_val,y_2)
+print("SGD: ",acc_2,"%")    
+
+w_3=_fit_mb_gd(x_train,y_train,0.2,0.1,500,10)
+y_3=_predict(x_val,w_3)
+acc_3=_accuracy(y_val,y_3)
+print("MB_GD: ",acc_3,"%")    
